7f7fbfd6162105c230ea7b9bbdebea51432f65a0
circinusX1 <xxxxx@xxxxx.com>
2021-03-24 00:04:56 +0000

    fix arm

--- bin/aplay.cs
+++ bin/aplay.cs
@@ -4,10 +4,13 @@
 /**
   check your ao library location
   */
-lib := LIB("/usr/lib/x86_64-linux-gnu/libao.so");
+// lib := LIB("/usr/lib/x86_64-linux-gnu/libao.so");
+lib := LIB("/usr/lib/arm-linux-gnueabihf/libao.so");
+
 
 lib.load("ao_initialize",false,0);
 lib.load("ao_default_driver_id",true,0);
+lib.load("ao_driver_id",true,1);
 lib.load("ao_open_live",true,3);
 lib.load("ao_play",false,3);
 lib.load("ao_close",0,1);
@@ -16,14 +19,22 @@ lib.load("ao_shutdown",0,0);
 function main(x)
 {
     // find a wav PCM file, ao does snot decode
-    var file   = FILE(eBINARY, "/home/marius/Music/iris.wav","iris");
+    var file   = FILE(eBINARY, "./iris.wav","iris");
     var buffer = BUFF([4096]);                  //   file buffer
     var format = BUFF(['i''i','i','i','v']);    // format structure int, int int int void
     format.set([16,44100,2,1,0]);               // populate structure
     
+    print(file + " file\n");
+
     ao_initialize();                            // init ao
     var default_driver = ao_default_driver_id();    
+//    var default_driver = ao_driver_id("hw:0");
+    
     var device = ao_open_live(default_driver, format.get(), 0);
+    
+    print(default_driver + " default_driver\n");
+    print(device + " device\n");
+
     if(device && file.open(1))
     {
         while(1)
@@ -36,6 +47,8 @@ function main(x)
         }
         file.close();
     }
+    else
+        println("no device or no mediafile");
     ao_close(device);
     ao_shutdown();
     println("done");
